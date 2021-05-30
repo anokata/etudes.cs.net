ea08d0c94423250b5c7656842f70ea8294eb06df
anokata <anokata8@mail.ru>
2021-05-30 12:44:58 +0300

    Add bare cmd app by dotnet new console

---
+++ barecmdapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace barecmdapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
