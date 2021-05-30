1314f385f6a5a57d23e0c03dd6178cf1322e7d9b
anokata <anokata8@mail.ru>
2021-05-30 10:17:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private const UInt32 param_v = 123213123;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter:");
+            string name = Console.ReadLine();
+            for (uint i = param_v; i < param_v+3; i++)
+            {
+                Console.WriteLine($"Hello {name} i is {i}");
+            }
+            
+        }
+    }
+}
