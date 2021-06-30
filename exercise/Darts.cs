02d59281ba4b7d5c0e0377c7a738ac818dce478c
anokata <anokata8@mail.ru>
2021-06-30 19:08:40 +0300

    [add] darts exercise.

---
+++ exercise/Darts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double distanceToCenter = Math.Sqrt(x*x + y*y);
+        if (distanceToCenter > 10) return 0;
+        if (distanceToCenter > 5) return 1;
+        if (distanceToCenter > 1) return 5;
+        return 10;
+    }
+}
