e8597428ff36eedafae7eabe446736b42f3f8e86
anokata <anokata8@mail.ru>
2021-07-02 17:17:40 +0300

    [add] exercise/Hamming.cs

---
+++ exercise/Hamming.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length) throw new ArgumentException();
+
+        int distance = 0;
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i]) distance++;
+        }
+        return distance;
+    }
+}
