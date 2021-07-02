00b7260da359d2d0c714a1e9907e2396b8d70278
anokata <anokata8@mail.ru>
2021-07-02 08:34:50 +0300

    [add] isogram.

---
+++ exercise/Isogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        bool []letters = new bool[26];
+        foreach (char letter in word.ToLower())
+        {
+            if (Char.IsLetter(letter)) 
+            {
+                if (letters[letter - 'a']) return false;
+                letters[letter - 'a'] = true;
+            }
+        }
+        return true;
+    }
+}
