6e367169f8ac8c0a0076f9f94f872c74e10ce5a6
anokata <anokata8@mail.ru>
2021-07-05 15:57:18 +0300

    [add] grains.cs

---
+++ exercise/Grains.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n) 
+    {
+        if (n > 64 || n < 1) throw new ArgumentOutOfRangeException();
+        return (ulong) Math.Pow(2, n-1);
+    }
+
+    public static ulong Total() => (ulong) (System.Numerics.BigInteger.Pow(2, 64) - 1);
+}
