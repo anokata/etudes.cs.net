cc1746f50bfd4071b07fd7e4b33128e993d693ca
anokata <anokata8@mail.ru>
2021-07-07 19:34:07 +0300

    [add] Pangram.cs

---
+++ exercise/Pangram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        if (input.Length < 26) return false;
+        bool[] lettersFlags = FlaggifyLetters(input.ToLower());
+
+        foreach (bool flag in lettersFlags) {
+            if (!flag) return false;
+        }
+
+        return true;
+    }
+
+    private static bool[] FlaggifyLetters(string input) {
+        bool[] lettersFlags = new bool[26];
+        foreach (char letter in input) {
+            if (Char.IsLetter(letter))
+                lettersFlags[letter - 'a'] = true;
+        }
+        return lettersFlags;
+    }
+}
