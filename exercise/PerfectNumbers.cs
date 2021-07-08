68405f91d26fc2f181fcb01d6407a55d5a1a0740
anokata <anokata8@mail.ru>
2021-07-08 07:30:45 +0300

    [add] PerfectNumbers.cs

---
+++ exercise/PerfectNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        if (number == 1) return Classification.Deficient;
+        int maxDivisor = (int)(number / 2);
+        int divisorsSum = 1;  // All dived by 1;
+        for (int divisor = 2; divisor <= maxDivisor; divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                divisorsSum += divisor;
+            }
+            if (divisorsSum > number) return Classification.Abundant;
+        }
+        if (divisorsSum == number) return Classification.Perfect;
+        return Classification.Deficient;
+    }
+}
