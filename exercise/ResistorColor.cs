c5bed42522851a7ec5137d9d9b9686c887f096e6
anokata <anokata8@mail.ru>
2021-07-09 07:33:41 +0300

    [add] ResistorColor.cs using Enums

---
+++ exercise/ResistorColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+enum ColorCodes 
+{
+    black = 0,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+}
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) => (int) Enum.Parse(typeof(ColorCodes), color.ToLower());
+
+    public static string[] Colors() => Enum.GetNames(typeof(ColorCodes));
+}
