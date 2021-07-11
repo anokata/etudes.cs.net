e150c7d8806ed9b94abcfeacfb3d57b09c82563b
anokata <anokata8@mail.ru>
2021-07-11 09:17:07 +0300

    [add] solved CollatzConjecture

---
+++ exercise/CollatzConjecture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        int steps = 0;
+        while (number != 1)
+        {
+            number = MakeStep(number);
+            steps++;
+        }
+        return steps;
+    }
+
+    private static int MakeStep(int number) 
+    {
+        if (number % 2 == 0) return number / 2;
+        return number * 3 + 1;
+    }
+}
