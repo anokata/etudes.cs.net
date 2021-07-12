01579a8d84760c5e268c3783022c41b787541182
anokata <anokata8@mail.ru>
2021-07-12 08:18:55 +0300

    [add] Pascal Triangle

---
+++ exercise/PascalsTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        int[][] triangle = new int[rows][];
+        for (int r = 0; r < rows; r++)
+        {
+            triangle[r] = new int[r+1];
+            for (int c = 0; c < r+1; c++)
+            {
+                triangle[r][c] = (int) binominal(r, c);
+            }
+        }
+        return triangle;
+    }
+
+    public static long binominal(int n, int k) => factorial(n)/(factorial(k)*factorial(n-k));
+
+    public static long factorial(int n)
+    {
+        long f = 1;
+        if (n < 2) return f;
+        for (int i = 2; i <= n; i++) f *= i;
+        return f;
+    }
+
+}
