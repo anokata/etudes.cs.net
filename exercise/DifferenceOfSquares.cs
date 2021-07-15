5c27b291c2cae17461d125ea2b51b5de3c65cad9
anokata <anokata8@mail.ru>
2021-07-15 17:38:49 +0300

    [add] Difference of squares

---
+++ exercise/DifferenceOfSquares.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        // Using collections
+        return (int) Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+        // Using Math and brain
+        //return (int) Math.Pow(((max+1)*max)/2, 2);
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        // Using collections
+        return Enumerable.Range(1, max).Select(x => x* x).Sum();
+        // Using Math and brain
+        //return ((2*max+1)*(max+1)*max)/6;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
