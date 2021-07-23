0b7b38abd8fdae3d9025f402f1c8662d1423ce4d
anokata <anokata8@mail.ru>
2021-07-23 18:14:41 +0300

    [add]  solve first prot: CircualrBuffer.cs

---
+++ exercise/CircularBuffer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private Queue<T> buf;
+    private int capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        buf = new Queue<T>(capacity);
+        this.capacity = capacity;
+    }
+
+    public T Read() => buf.Dequeue();
+
+    public void Write(T value)
+    {
+        if (capacity == buf.Count)
+            throw new InvalidOperationException("Not enough capacity");
+        buf.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (buf.Count == capacity)
+            Clear();
+        Write(value);
+    }
+
+    public void Clear()
+    {
+        if (buf.Count > 0)
+            buf.Dequeue();
+    }
+}
