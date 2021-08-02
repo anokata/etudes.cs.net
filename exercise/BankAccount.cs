f9ec7219bed12f46ca46088eef04a63eda2f4b39
anokata <anokata8@mail.ru>
2021-08-02 09:05:19 +0300

    [add] BankAccount.cs

---
+++ exercise/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class BankAccount
+{
+
+    private decimal balance = 0;
+    private bool opened = false;
+    private readonly object locker = new Object();
+
+    public void Open()
+    {
+        lock (locker)
+        {
+            opened = true;
+        }
+    }
+
+    public void Close()
+    {
+        lock (locker)
+        {
+            opened = false;
+        }
+    }
+
+    public decimal Balance
+    {
+        get  {
+            lock (locker)
+            {
+                if (!opened) throw new InvalidOperationException("Account is closed.");
+                return balance;
+            }
+        }
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+        lock (locker)
+        {
+            balance += change;
+        }
+    }
+}
