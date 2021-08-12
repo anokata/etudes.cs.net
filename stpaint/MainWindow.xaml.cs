9c08c6fc46877e771c57c820b861d40597da7a13
anokata <anokata8@mail.ru>
2021-08-12 19:17:01 +0300

    [chg] xaml..

--- stpaint/MainWindow.xaml.cs
+++ stpaint/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace stpaint
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click!");
+        }
     }
 }
